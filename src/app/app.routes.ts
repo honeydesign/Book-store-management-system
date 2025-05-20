81be79a99aa78e0ae8c91e4ff8c93e556363d8ba
orunsolukafilatoyinade <orunsolukafilat@gmail.com>
2025-05-20 13:21:40 +0100

    admin role

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { OrderComponent } from './order/order.component';
 import { CustomersComponent } from './customers/customers.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 export const routes: Routes = [
@@ -17,7 +18,8 @@ export const routes: Routes = [
     { path: 'orders', component: OrderComponent },
     { path: 'customers', component: CustomersComponent },
     { path: 'transactions', component: TransactionComponent },
-    { path: 'add-products', component: AddProductComponent }
+    { path: 'add-products', component: AddProductComponent },
+    { path: 'admin-role', component: AdminComponent }
 
 
 ];
